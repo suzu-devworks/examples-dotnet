e922ea56893e5e235344955e4997b5f2dfdbb3c0
A.suzuki <suzu.devworks@gmail.com>
2023-11-29 14:20:36 +0000

    Copies IoC Containers examples.

---
+++ tests/Examples.DependencyInjection.IoC.Tests/DependencyInjection.Tests/ConstructorSelectionTests.cs
@@ -0,0 +1,36 @@
+using Examples.DependencyInjection.IoC.Tests;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Examples.DependencyInjection.Tests;
+
+/// <summary>
+/// Tests to study dependency injection using <see cref="Microsoft.Extensions.DependencyInjection" />.
+/// Multiple Contractor selection.
+/// </summary>
+public partial class ConstructorSelectionTests
+{
+
+    [Fact]
+    public void WhenCallingGetService_WithMultipleConstructor()
+    {
+        var mock = new Mock<IVerifier>();
+        mock.Setup(x => x.Called("Hello DI world!!!"));
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IVerifier>(_ => mock.Object);
+        services.AddSingleton<ClassWithMultipleConstructors>(provider =>
+            new ClassWithMultipleConstructors(provider.GetRequiredService<IVerifier>()));
+
+        using var provider = services.BuildServiceProvider();
+
+        // injection IEnumerable<IMessageGenerator>.
+        var single = provider.GetService<ClassWithMultipleConstructors>();
+        single!.Verify();
+
+        mock.Verify(x => x.Called("Hello DI world!!!"), Times.Once());
+        mock.VerifyNoOtherCalls();
+        return;
+    }
+
+
+}
--- tests/Examples.DependencyInjection.IoC.Tests/GlobalUsings.cs
+++ tests/Examples.DependencyInjection.IoC.Tests/GlobalUsings.cs
@@ -1,1 +1,3 @@
+global using ChainingAssertion;
+global using Moq;
 global using Xunit;
