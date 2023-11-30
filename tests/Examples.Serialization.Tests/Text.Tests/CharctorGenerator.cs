fd840396b927efbd971f3f8da101f63bd4487778
A.suzuki <suzu.devworks@gmail.com>
2023-11-30 14:27:21 +0000

    Copies Serialization examples.

---
+++ tests/Examples.Serialization.Tests/Text.Tests/CharctorGenerator.cs
@@ -0,0 +1,22 @@
+namespace Examples.Text.Tests;
+
+internal static class CharacterGenerator
+{
+    public static IEnumerable<char> EnumerateHiragana()
+        => EnumerateCharacters('\u3041', '\u309F');
+
+    public static IEnumerable<char> EnumerateFullWidthKatakana()
+        => EnumerateCharacters('\u30A0', '\u30FF');
+
+    public static IEnumerable<char> EnumerateHalfWidthKatakana()
+        => EnumerateCharacters('\uFF61', '\uFF9F');
+
+    private static IEnumerable<char> EnumerateCharacters(char start, char end)
+    {
+        foreach (var c in Enumerable.Range(start, end - start + 1))
+        {
+            yield return Convert.ToChar(c);
+        }
+    }
+
+}
