0059c8accdda4a6912be200602218134b0d46cba
A.suzuki <suzu.devworks@gmail.com>
2026-07-14 09:20:29 +0000

    feat(devcontainer): add graphics, mssql, pgsql, and web configurations with necessary scripts and setup

--- .devcontainer/graphics/postCreateCommand.sh
+++ .devcontainer/graphics/postCreateCommand.sh
@@ -6,4 +6,5 @@ echo "USER:" ${script_user}
 echo "DIR:" ${script_dir}
 echo
 
-sh ${script_dir}/scripts/install-xunit3template.sh
+sh ${script_dir}/../scripts/install-xunit3template.sh
+sh ${script_dir}/../scripts/download-fonts.sh
